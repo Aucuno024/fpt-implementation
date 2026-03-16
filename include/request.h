6f4cbd1e238e4133539a628d3ca2d35b92c48b0a
aucuno <aucuno2012@gmail.com>
2026-03-16 15:31:38 +0100

    Definissions des structures de requetes
    include/request.h (feat) Ajout d'un type enumere pour le type de requete et ajout d'une structure
    de requetes.

---
+++ include/request.h
@@ -0,0 +1,15 @@
+#ifndef __REQUEST_H__
+#define __REQUEST_H__
+#include "csapp.h"
+typedef enum {
+    GET = 0,
+    PUT = 1,
+    LS = 2,
+    RM = 3
+} typereq_t;
+
+typedef struct {
+    typereq_t typereq;
+    char path[MAXLINE];
+} request_t;
+#endif
